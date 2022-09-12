ac3f562f59bdac4626233a146814ac452aeea8d7
Marty Nelson <marty@martypdx.com>
2022-09-12 10:49:39 -0700

    events to actions

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 /* Imports */
 
 /* State */
-let name = 'Joe';
+let name = 'dog';
 
 /* Actions */
 function loadPage() {
+    displayNameTag();
+}
+
+function updateName(newName) {
+    name = newName;
     displayNameTag();
 }
 
@@ -15,6 +20,8 @@ function loadPage() {
 /* NameTag */
 // get DOM
 const nameDisplay = document.getElementById('name-display');
+const nameInput = document.getElementById('name-input');
+const updateButton = document.getElementById('update-button');
 
 // display
 function displayNameTag() {
@@ -22,6 +29,13 @@ function displayNameTag() {
 }
 
 // event listeners
+updateButton.addEventListener('click', () => {
+    // get the new name value
+    const newName = nameInput.value;
+
+    // call action
+    updateName(newName);
+});
 
 /* Run page load code */
 loadPage();
