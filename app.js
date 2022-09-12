fc7e6a65014a2d86faebef3637589dd575d20343
Marty Nelson <marty@martypdx.com>
2022-09-12 11:45:04 -0700

    reset main app page to do coin flip instead of name tag

--- app.js
+++ app.js
@@ -1,41 +1,16 @@
 /* Imports */
 
 /* State */
-let name = 'dog';
 
 /* Actions */
-function loadPage() {
-    displayNameTag();
-}
-
-function updateName(newName) {
-    name = newName;
-    displayNameTag();
-}
+function loadPage() {}
 
 /* Components */
 
 /* Component */
-
-/* NameTag */
 // get DOM
-const nameDisplay = document.getElementById('name-display');
-const nameInput = document.getElementById('name-input');
-const updateButton = document.getElementById('update-button');
-
 // display
-function displayNameTag() {
-    nameDisplay.textContent = name;
-}
-
 // event listeners
-updateButton.addEventListener('click', () => {
-    // get the new name value
-    const newName = nameInput.value;
-
-    // call action
-    updateName(newName);
-});
 
 /* Run page load code */
 loadPage();
