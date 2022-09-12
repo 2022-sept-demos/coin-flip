cf4827f4d798e93dd635838b8d07f8af748cbb28
Marty Nelson <marty@martypdx.com>
2022-09-12 10:26:43 -0700

    display name tag from state

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
 /* Imports */
 
 /* State */
+let name = 'Joe';
 
 /* Actions */
-function loadPage() {}
+function loadPage() {
+    displayNameTag();
+}
 
 /* Components */
 
 /* Component */
+
+/* NameTag */
 // get DOM
+const nameDisplay = document.getElementById('name-display');
+
 // display
+function displayNameTag() {
+    nameDisplay.textContent = name;
+}
+
 // event listeners
 
 /* Run page load code */
